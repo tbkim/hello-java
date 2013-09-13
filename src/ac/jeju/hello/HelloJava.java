d10bf1ba3acd1b8cec2d0a508b588f75e364be5d
app-23 <jnuktb@hanmail.net>
2013-09-13 17:06:47 +0900

    test commit

---
+++ src/ac/jeju/hello/HelloJava.java
@@ -0,0 +1,14 @@
+package ac.jeju.hello;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: app-23
+ * Date: 13. 9. 13.
+ * Time: 오후 4:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("HEllo JAVA");
+    }
+}
